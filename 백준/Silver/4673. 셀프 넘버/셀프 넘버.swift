68552c423db382c44e92c7e63259cc13113d3a61
luninj <103933747+luninj@users.noreply.github.com>
2022-06-24 00:28:38 +0900

    [Silver V] Title: 셀프 넘버, Time: 12 ms, Memory: 69096 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.swift
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.swift
@@ -1,27 +1,11 @@
 var selfNum : [Int] = Array(repeating: 1, count: 10000)
 
 for i in 1...10000 {
-    if i / 10 == 0 {
-        selfNum[i+i-1] = 0
-    }
-    
-    else if i / 100 == 0 && i / 10 != 0 {
-        selfNum[i + (i / 10) + (i % 10) - 1] = 0
-    }
-    
-    else if i / 1000 == 0 && i / 100 != 0 {
-        selfNum[i + (i / 100) + ((i / 10) % 10) + (i % 10) - 1] = 0
-    }
-    
-    else if i / 10000 == 0 && i / 1000 != 0 {
-        let temp = i + (i / 1000) + ((i / 100) % 10) + ((i / 10) % 10) + (i % 10) - 1
-        if temp < 10000 {
-            selfNum[temp] = 0
-        }
+    var temp = i + (i / 1000 % 10) + (i / 100 % 10) + (i / 10 % 10) + (i % 10) - 1
+    if temp < 10000 {
+        selfNum[temp] = 0
     }
 }
-
-
 
 for i in 0...9999 {
     if selfNum[i] == 1 {
