c3c8dd70da9c1f91c0387155f2640e27b17735a2
luninj <103933747+luninj@users.noreply.github.com>
2022-07-06 19:13:16 +0900

    [Silver III] Title: 2×n 타일링, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.swift
@@ -0,0 +1,23 @@
+var N = Int(readLine()!)!
+
+var dp = Array(repeating: 0, count: N)
+
+if N == 1 {
+    print(1)
+}
+
+else if N == 2 {
+    print(2)
+}
+
+else {
+    dp[0] = 1
+    dp[1] = 2
+    
+    for i in 2..<N {
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007
+    }
+    
+    print(dp[N-1] % 10007)
+}
+
